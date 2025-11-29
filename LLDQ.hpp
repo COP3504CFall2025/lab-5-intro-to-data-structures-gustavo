12fe91555209c6d6fff9b3609f44da5e6e78ac21
Gustavo Esteban Falcon <gestebanfalcon@gmail.com>
2025-11-29 16:53:08 -0500

    lldq

--- LLDQ.hpp
+++ LLDQ.hpp
@@ -6,10 +6,9 @@
 #include "LinkedList.hpp"
 #include <utility>
 
-
-
 template <typename T>
-class LLDQ : public DequeInterface<T> {
+class LLDQ : public DequeInterface<T>
+{
 private:
     LinkedList<T> list;
 
@@ -18,23 +17,56 @@ public:
     LLDQ();
 
     // Core Insertion Operations
-    void pushFront(const T& item) override;
-    void pushBack(const T& item) override;
+    void pushFront(const T &item) override
+    {
+        this->list.addHead(item);
+    }
+    void pushBack(const T &item) override
+    {
+        this->list.addTail(item);
+    }
 
     // Core Removal Operations
-    T popFront() override;
-    T popBack() override;
+    T popFront() override
+    {
+        T *pHead = this->list.getHead();
+        if (!pHead)
+            throw std::out_of_range("no head vro");
+        T head = *pHead;
+        this->list.removeHead();
+        return head;
+    }
+    T popBack() override
+    {
+        T *pTail = this->list.getTail();
+        if (!pTail)
+            throw std::out_of_range("no tail vro");
+        T tail = *pTail;
+        this->list.removeTail();
+        return tail;
+    }
 
     // Element Accessors
-    const T& front() const override;
-    const T& back() const override;
+    const T &front() const override
+    {
+        T *pHead = this->list.getHead();
+        if (!pHead)
+            throw std::out_of_range("no head vro");
+        T head = *pHead;
+        return head;
+    }
+    const T &back() const override
+    {
+        T *pTail = this->list.getTail();
+        if (!pTail)
+            throw std::out_of_range("no tail vro");
+        T tail = *pTail;
+        return tail;
+    }
 
     // Getter
-    std::size_t getSize() const noexcept override;
+    std::size_t getSize() const noexcept override
+    {
+        return this->list.getCount();
+    }
 };
-
-
-
-
-
-
