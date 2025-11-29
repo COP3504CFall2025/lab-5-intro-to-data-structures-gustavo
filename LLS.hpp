5eb22a8dbb5f5cd7a5f4d5cb3bccfb7a6eb899d3
Gustavo Esteban Falcon <gestebanfalcon@gmail.com>
2025-11-29 16:45:52 -0500

    lls

--- LLS.hpp
+++ LLS.hpp
@@ -6,22 +6,45 @@
 #include <stdexcept>
 
 template <typename T>
-class LLS : public StackInterface<T> {
+class LLS : public StackInterface<T>
+{
 private:
     LinkedList<T> list;
+
 public:
     // Constructor
-    LLS();
+    LLS() : list() {}
 
     // Insertion
-    void push(const T& item) override;
+    void push(const T &item) override
+    {
+        this->list.addHead(item);
+    }
 
     // Deletion
-    T pop() override;
+    T pop() override
+    {
+        T *pHead = this->list.getHead();
+        if (!pHead)
+            throw std::out_of_range("no head vro");
+        T head = *pHead;
+        this->list.removeHead();
+        return head;
+    }
 
     // Access
-    T peek() const override;
+    T peek() const override
+    {
+        T *pHead = this->list.getHead();
+        if (!pHead)
+            throw std::out_of_range("no head vro");
+        T head = *pHead;
+        return head;
+    }
 
-    //Getters
-    std::size_t getSize() const noexcept override;
+    // Getters
+    std::size_t getSize() const noexcept override
+    {
+        return this->list.getCount();
+    }
 };
