50fad40a391f87bc41c1c5b37e00f9df9c4fe2e6
Gustavo Esteban Falcon <gestebanfalcon@gmail.com>
2025-11-29 16:40:00 -0500

    llq

--- LLQ.hpp
+++ LLQ.hpp
@@ -6,23 +6,45 @@
 #include <stdexcept>
 
 template <typename T>
-class LLQ : public QueueInterface<T> {
+class LLQ : public QueueInterface<T>
+{
 private:
     LinkedList<T> list;
+
 public:
     // Constructor
-    LLQ();
+    LLQ() : list() {};
 
     // Insertion
-    void enqueue(const T& item) override;
+    void enqueue(const T &item) override
+    {
+        this->list.addHead(item);
+    }
 
     // Deletion
-    T dequeue() override;
+    T dequeue() override
+    {
+        T *pTail = this->list.getTail();
+        if (!pTail)
+            throw std::out_of_range("no tail vro");
+        T tail = *pTail;
+        this->list.removeTail();
+        return tail;
+    }
 
     // Access
-    T peek() const override;
+    T peek() const override
+    {
+        T *pTail = this->list.getTail();
+        if (!pTail)
+            throw std::out_of_range("no tail vro");
+        T tail = *pTail;
+        return tail;
+    }
 
     // Getter
-    std::size_t getSize() const noexcept override;
-
+    std::size_t getSize() const noexcept override
+    {
+        return this->list.getCount();
+    }
 };
